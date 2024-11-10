abbfb7c9c2e130355d7dfaa8bb34f48f52e5a21b
Yry01 <103943433+Yry01@users.noreply.github.com>
2024-11-10 15:40:58 -0500

    Setup Backend Environment

---
+++ backend/src/test/java/com/projects/aichess/AichessApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projects.aichess;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AichessApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
