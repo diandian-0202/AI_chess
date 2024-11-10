b996148f8c1fdd0d9275831086c6e2d55335f11b
Yry01 <103943433+Yry01@users.noreply.github.com>
2024-11-10 14:21:02 -0500

    Initialize backend

---
+++ backend/aichess/aichess/src/main/java/com/projects/aichess/AichessApplication.java
@@ -0,0 +1,15 @@
+package com.projects.aichess;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AichessApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AichessApplication.class, args);
+		System.out.println("Hello World");
+
+	}
+
+}
