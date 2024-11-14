fa049e93fc222234bb44b99f7193890cfef223fd
Wentinggg <wentingz@seas.upenn.edu>
2024-11-13 19:28:02 -0800

    Add Override in UserEntity.java

--- backend/src/main/java/com/projects/aichess/db/entity/UserEntity.java
+++ backend/src/main/java/com/projects/aichess/db/entity/UserEntity.java
@@ -6,6 +6,8 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 
+import java.util.Objects;
+
 @Entity
 public class UserEntity {
     @Id
@@ -42,6 +44,31 @@ public class UserEntity {
 
     public void setPassword(String password) {
         this.password = password;
+    }
+
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        UserEntity that = (UserEntity) o;
+        return Objects.equals(id, that.id) &&
+                Objects.equals(name, that.name) &&
+                Objects.equals(password, that.password);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, password);
+    }
+
+    @Override
+    public String toString() {
+        return "UserEntity{" +
+                "id=" + id +
+                ", name=" + name + '\'' +
+                ", password='" + password + '\'' +
+                '}';
     }
 
 }
